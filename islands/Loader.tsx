348990c9512864001f86fd329c73e03698ceaabb
zhuzhenfeng.code <zhuzhenfeng.code@bytedance.com>
2022-10-24 23:17:41 +0800

    support upload to chain
    Change-Id: I807cfe5ac4d97b54074d9418965e736488ab27fc

--- islands/Loader.tsx
+++ islands/Loader.tsx
@@ -5,35 +5,59 @@ import * as gfm from "https://deno.land/x/gfm@0.1.23/mod.ts";
 import axiod from "https://deno.land/x/axiod/mod.ts";
 import { Database } from "../commuction/database.ts";
 import { Func } from "../utils/types.ts";
+import { ethers, providers } from "https://cdn.skypack.dev/ethers";
+import { contract } from "../commuction/contract.ts";
+
+const db = new Database();
+async function addToPg(files: any, gist_id: string) {
+  const funcs = new Array<Func>();
+  for (let key of Object.keys(files)) {
+    funcs.push(
+      {
+        token_id: 1,
+        chain_type: "polygon",
+        content: files[key].content,
+        txn_hash: "",
+        source_gist_id: gist_id,
+      },
+    );
+  }
+
+  await db.insert(funcs);
+}
 
 export default function MLoader() {
   const [gist, setGist] = useState("");
-  const db = new Database();
+
+  const { ethereum } = window;
 
   return (
     <div class="rounded-2xl w-5/6 md:w-5/12 max-w-xl pt-4 pb-8 px-7">
       <form
         onSubmit={async (e) => {
           e.preventDefault();
+          //   51ceeefe042a23dc6c0f218ec415ff16
+
           const res = await axiod.get(`https://api.github.com/gists/${gist}`);
           console.log(res.data);
           const files = res.data.files;
-          const funcs = new Array<Func>();
-          for (let key of Object.keys(files)) {
-            funcs.push(
-              {
-                token_id: 1,
-                chain_type: "polygon",
-                content: files[key].content,
-                txn_hash: "",
-                source_gist_id: gist,
-              },
-            );
-          }
+          //   await addToPg(files, gist);
+          const nfts = await contract.purpose();
+          console.log(nfts);
 
-          //   51ceeefe042a23dc6c0f218ec415ff16
-          await db.insert(funcs)
-          alert("publish ok")
+          const provider = new providers.Web3Provider(ethereum);
+          await provider.send("eth_requestAccounts", []);
+          const signer = provider.getSigner();
+          const c = contract.connect(signer);
+
+        //   const a = await c.mintWithMeta(JSON.stringify(files));
+        //   console.log(a)
+          const meta = await c.faasCodes(
+            "0x80062eE8E85fD91D82EdDdfa059f05b11863768E",
+            3,
+          );
+
+          alert(meta);
         }}
       >
         <label>
