d0fccf0a24abedc110a2e4ae1f5093a361af5364
zhenfeng-zhu <codeless98@163.com>
2023-01-30 22:39:20 +0800

    add local cache and inner sdk

--- routes/api/moverun.ts
+++ routes/api/moverun.ts
@@ -1,28 +1,39 @@
-import { HandlerContext } from "$fresh/server.ts"
-import { importString } from "https://deno.land/x/import@v0.1.6/mod.ts"
-import { Database } from "../../utils/database.ts"
+import { HandlerContext } from "$fresh/server.ts";
+import { importString } from "https://deno.land/x/import@v0.1.7/mod.ts";
+import { Database } from "../../utils/database.ts";
+import * as aptos from "npm:aptos";
 
-const db = new Database()
+const __innerSDK = { aptos };
 
+const db = new Database();
 export const handler = async (
   _req: Request,
-  _ctx: HandlerContext,
+  _ctx: HandlerContext
 ): Promise<Response> => {
   if (_req.method != "POST") {
-    return Response.json({ error: "only support post" })
+    return Response.json({ error: "only support post" });
   }
 
-  const payload = await _req.json()
-  console.log(`payload: ${payload}`)
-  const data = await db.find_by_name(payload.name)
+  const payload = await _req.json();
+  console.log(`payload: `);
+  console.log(payload);
+  const data = await db.find_with_cache(payload.name);
 
-  const func = data[0]
+  const func = data[0];
   if (func.name != payload.name) {
-    return Response.json({ error: "name is not equal" })
+    return Response.json({ error: "name is not equal" });
   }
 
-  console.log(func.content)
-  const mod = await importString(func.content)
-  const result = await mod.handler()
-  return Response.json(result)
-}
+  console.log("function content: ");
+  console.log(func.content);
+
+  console.log("\n--------function log start---------");
+  const mod = await importString(func.content);
+  const result = await mod.handler(payload, __innerSDK);
+  console.log("--------function log end-----------\n");
+
+  console.log("function result: ");
+  console.log(result);
+
+  return Response.json(result);
+};
